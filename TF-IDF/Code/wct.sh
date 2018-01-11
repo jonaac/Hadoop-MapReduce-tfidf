520e6339570cb4287ec90a76bc7579cf60641aab
Jonathan Azpur <jazpurc@gmail.com>
2018-01-10 20:10:09 -0500

    Update wct.sh

--- TF-IDF/Code/wct.sh
+++ TF-IDF/Code/wct.sh
@@ -11,7 +11,7 @@ bin/hadoop jar $STREAMINGJAR                        \
     -output  Output1
 
 printf "\nMAPPER-REDUCER #2\n\n"
-bin/hadoop jar $STREAMINGJAR						\
+bin/hadoop jar $STREAMINGJAR                        \
     -files   $WCDIR/mapper2.py,$WCDIR/reducer2.py 	\
     -mapper  $WCDIR/mapper2.py						\
     -reducer $WCDIR/reducer2.py                     \
@@ -20,13 +20,13 @@ bin/hadoop jar $STREAMINGJAR						\
 
 
 printf "\nMAPPER-REDUCER #3\n\n"
-bin/hadoop jar $STREAMINGJAR			     															\
-    -D mapreduce.job.output.key.comparator.class=org.apache.hadoop.mapred.lib.KeyFieldBasedComparator 	\
-    -D stream.num.map.output.key.fields=2            													\
-    -D mapreduce.partition.keycomparator.options="-k1,1 -k2,2" 											\
-    -D mapreduce.partition.keypartitioner.options="-k1,1"   											\
-    -files   $WCDIR/mapper3.py,$WCDIR/reducer3.py    													\
-    -mapper  $WCDIR/mapper3.py                       													\
-    -reducer $WCDIR/reducer3.py                      													\
-    -input   Output2/'*'                            													\
+bin/hadoop jar $STREAMINGJAR                                                                            \
+    -D mapreduce.job.output.key.comparator.class=org.apache.hadoop.mapred.lib.KeyFieldBasedComparator   \
+    -D stream.num.map.output.key.fields=2                                                               \
+    -D mapreduce.partition.keycomparator.options="-k1,1 -k2,2"                                          \
+    -D mapreduce.partition.keypartitioner.options="-k1,1"                                               \
+    -files   $WCDIR/mapper3.py,$WCDIR/reducer3.py                                                       \
+    -mapper  $WCDIR/mapper3.py                                                                          \
+    -reducer $WCDIR/reducer3.py                                                                         \
+    -input   Output2/'*'                                                                                \
     -output  Output3
