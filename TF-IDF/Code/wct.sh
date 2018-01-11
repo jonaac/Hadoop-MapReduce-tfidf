30566ca4991156885e5b1e217497eea9dc1f0c23
Jonathan Azpur <jazpurc@gmail.com>
2018-01-10 19:40:13 -0500

    Update wct.sh

--- TF-IDF/Code/wct.sh
+++ TF-IDF/Code/wct.sh
@@ -8,5 +8,25 @@ bin/hadoop jar $STREAMINGJAR                        \
     -mapper  $WCDIR/wordMap.py                      \
     -reducer $WCDIR/wordReduce.py                   \
     -input   Gutenberg/'*'                          \
-    -output  Gutenberg-out
+    -output  Output1
 
+printf "\nMAPPER-REDUCER #2\n\n"
+bin/hadoop jar $STREAMINGJAR						\
+    -files   $WCDIR/mapper2.py,$WCDIR/reducer2.py 	\
+    -mapper  $WCDIR/mapper2.py						\
+    -reducer $WCDIR/reducer2.py                     \
+    -input   Output1/'*'							\
+    -output  Output2
+
+
+printf "\nMAPPER-REDUCER #3\n\n"
+bin/hadoop jar $STREAMINGJAR			     															\
+    -D mapreduce.job.output.key.comparator.class=org.apache.hadoop.mapred.lib.KeyFieldBasedComparator 	\
+    -D stream.num.map.output.key.fields=2            													\
+    -D mapreduce.partition.keycomparator.options="-k1,1 -k2,2" 											\
+    -D mapreduce.partition.keypartitioner.options="-k1,1"   											\
+    -files   $WCDIR/mapper3.py,$WCDIR/reducer3.py    													\
+    -mapper  $WCDIR/mapper3.py                       													\
+    -reducer $WCDIR/reducer3.py                      													\
+    -input   Output2/'*'                            													\
+    -output  Output3
